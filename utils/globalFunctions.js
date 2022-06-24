9cf678f8285cbc3b535c9ca119cbd9f4ae5302ad
Akshay Chaturvedi <akshay.chaturvedi@vaahaq.com>
2022-06-25 03:44:48 +0530

    change time formatting

--- utils/globalFunctions.js
+++ utils/globalFunctions.js
@@ -1,3 +1,13 @@
-export const msToTime = data => {
-  return new Date(data).toISOString().slice(11, 19);
+export const msToTime = ms => {
+  const seconds = Math.floor((ms / 1000) % 60);
+  const minutes = Math.floor((ms / 1000 / 60) % 60);
+  const hours = Math.floor((ms / 1000 / 3600) % 24);
+
+  const humanized = [
+    hours.toString(),
+    minutes.toString(),
+    seconds.toString()
+  ].join(':');
+
+  return humanized;
 };
