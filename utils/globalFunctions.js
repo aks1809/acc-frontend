9d383580144747d00bf2369ee88d8063809e61c4
Akshay Chaturvedi <akshay.chaturvedi@vaahaq.com>
2022-06-25 03:16:53 +0530

    change time formatting

--- utils/globalFunctions.js
+++ utils/globalFunctions.js
@@ -1,6 +1,16 @@
-import { intervalToDuration } from 'date-fns';
+export const msToTime = ms => {
+  // 1- Convert to seconds:
+  let seconds = ms / 1000; // 2- Extract hours:
+  const hours = parseInt(seconds / 3600, 10); // 3600 seconds in 1 hour
+  seconds = parseInt(seconds % 3600, 10); // extract the remaining seconds after extracting hours // 3- Extract minutes:
 
-export const msToTime = duration => {
-  const obj = intervalToDuration({ start: 0, end: duration });
-  return `${obj.hours}:${obj.minutes}:${obj.seconds}`;
+  const minutes = parseInt(seconds / 60, 10); // 60 seconds in 1 minute // 4- Keep only seconds not extracted to minutes:
+
+  seconds = parseInt(seconds % 60, 10); // 5 - Format so it shows a leading zero if needed
+
+  const hoursStr = `00${hours}`.slice(-2);
+  const minutesStr = `00${minutes}`.slice(-2);
+  const secondsStr = `00${seconds}`.slice(-2);
+
+  return `${hoursStr}:${minutesStr}:${secondsStr}`;
 };
